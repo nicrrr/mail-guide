c3ecbf636553c2034d9ed8538c3d9f7c8a62a722
nicrrr <972790411@qq.com>
2018-06-01 10:47:55 +0800

    add code

---
+++ wm-yongo-parent/yougo-log-factory/src/main/java/yougo/log/factory/App.java
@@ -0,0 +1,13 @@
+package yougo.log.factory;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
